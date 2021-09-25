467e401fae029acf6dbcd4a935d8102ff55fcc64
Savannah Jackson <savannahjackson33@gmail.com>
2021-09-25 09:28:09 -0400

    Saturday class

---
+++ 04_SwitchCases/04_SwitchCases/04_SwitchCases.cs
@@ -0,0 +1,49 @@
+﻿using System
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_SwitchCases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 1;
+
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("Hello World!");
+                    break;
+                case 2:
+                    Console.WriteLine("What are you doing?");
+                    break;
+                default:
+                    Console.WriteLine("This is deafault. It will be executed if no other case is evaluated.");
+                    break;
+            }
+
+            DayOfWeek today = DateTime.Today.DayOfWeek;
+            switch (today)
+            {
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Welcome to the beginning of a work week!");
+                    break;
+                case: DayOfWeek.Tuesday:
+                case DayOfWeek.Wednesday:
+                case DayOfWeek.Thursday:
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Still working!");
+                    break;
+                case DayOfWeek.Saturday:
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("It's the weekend!!!");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
